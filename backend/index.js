91a23952456892dd6b85520dd8e4578c66ee0201
Paras <paraskamliya5@gmail.com>
2023-10-31 18:53:10 +0530

    user route created

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { userRouter } = require("./routes/user.router");
+const { adminRoute } = require("./routes/admin.router");
+const { connection } = require("./db");
+app.use(express.json());
+app.use(cors());
+app.use("/users", userRouter);
+app.use("/admin", adminRoute);
+
+app.get("/", (req, res) => {
+    res.status(200).send("Welcome to Home Page.")
+})
+
+app.listen(process.env.PORT, async () => {
+    try {
+        await connection;
+        console.log("Connected to DB");
+    } catch (error) {
+        console.log(error);
+    }
+})
---
+++ backend/routes/admin.router.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const { UserModel } = require("../model/user.model");
+const adminRoute = express.Router();
+
+adminRoute.get("/users", async (req, res) => {
+    try {
+        const data = await UserModel.find();
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(400).send({ "msg": "Something went wrong.", "err": error });
+    }
+})
+
+adminRoute.patch("/users/update/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        await UserModel.findByIdAndUpdate({ _id: id }, req.body);
+        res.status(200).send({ "msg": "User is updated successfully." });
+    } catch (error) {
+        res.status(400).send({ "msg": "Something went wrong.", "err": error });
+    }
+})
+
+adminRoute.delete("/users/delete/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        await UserModel.findByIdAndDelete({ _id: id });
+        res.status(200).send({ "msg": "User is deleted successfully." });
+    } catch (error) {
+        res.status(400).send({ "msg": "Something went wrong.", "err": error });
+    }
+})
+
+module.exports = { adminRoute };
