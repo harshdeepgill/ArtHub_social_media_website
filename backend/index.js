a45e0e30fe49b64e0b73e67afa2be9b50941c0eb
Paras <paraskamliya5@gmail.com>
2023-11-01 10:04:33 +0530

    update route

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,12 @@ require("dotenv").config();
 const { userRouter } = require("./routes/user.router");
 const { adminRoute } = require("./routes/admin.router");
 const { connection } = require("./db");
+const { artRouter } = require("./routes/arts.router");
 app.use(express.json());
 app.use(cors());
 app.use("/users", userRouter);
 app.use("/admin", adminRoute);
-
+app.use("/arts", artRouter);
 app.get("/", (req, res) => {
     res.status(200).send("Welcome to Home Page.")
 })
@@ -23,4 +24,3 @@ app.listen(process.env.PORT, async () => {
         console.log(error);
     }
 })
-
