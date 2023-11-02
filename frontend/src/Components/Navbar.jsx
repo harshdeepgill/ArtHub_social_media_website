23e710cee15e88f55153864d71de0df97980eebb
Paras <paraskamliya5@gmail.com>
2023-11-02 21:30:01 +0530

    update

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -10,13 +10,13 @@ const Navbar = () => {
     const changeTheme = () => {
         setTheme(theme == "dark" ? "light" : "dark")
     }
-    return <Box w={"100%"} bgColor={theme == "dark" ? "#424242" : "#bbbbbb"} color={theme == "dark" ? "white" : "black"}>
+    return <Box w={"100%"} bgColor={theme == "dark" ? "#424242" : "#dddddd"} color={theme == "dark" ? "white" : "black"}>
         <Stack w={"90%"} m={"auto"} direction={"row"} display={"flex"} justifyContent={"space-between"} alignItems={"center"} p={"1"}>
             <Image src="https://firebasestorage.googleapis.com/v0/b/festive-crayon.appspot.com/o/Posts%2FCraft_Ideas1-removebg.png029b92d9-75e9-48d1-b100-5b790f007a6c?alt=media&token=b496407b-289a-45d6-952d-9bb82d176b81" alt="logo" objectFit={"cover"} w={"10%"} />
             <InputGroup w={"30%"}>
-                <Input value={search} placeholder="Search" focusBorderColor="coral" onChange={(e) => setSearch(e.target.value)} />
+                <Input value={search} placeholder="Search" focusBorderColor="coral" borderColor={"#FF7F50"} _hover={{ borderColor: "none" }} onChange={(e) => setSearch(e.target.value)} />
                 <InputRightElement >
-                    <IconButton icon={<SearchIcon />} size={"sm"} p={"1"} />
+                    <IconButton icon={<SearchIcon />} size={"sm"} p={"1"} bgColor={"#8FDBA7"} _hover={{ bgColor: "none" }} />
                 </InputRightElement>
             </InputGroup>
             <Box display={"flex"} justifyContent={"space-between"} w={"16%"} alignItems={"center"}>
