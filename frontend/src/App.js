6e0ed266967adc6ae154959d94827e28d7d6a8f4
Paras <paraskamliya5@gmail.com>
2023-11-03 22:00:46 +0530

    auth update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import AllRoutes from "./Components/AllRoutes";
 import Navbar from "./Components/Navbar";
+import ArtPage from "./Pages/Artpage";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <AllRoutes />
+      <ArtPage />
     </div>
   );
 }
