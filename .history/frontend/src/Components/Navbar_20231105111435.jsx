529ae644ad7a3262b4eedc6814a562e2bc4d28fd
Ravi Theja <kopparapuraviteja@gmail.com>
2023-11-05 11:16:12 +0530

    added theme colors to home hero section

--- .history/frontend/src/Components/Navbar_20231105111435.jsx
+++ .history/frontend/src/Components/Navbar_20231105111435.jsx
@@ -1,6 +1,6 @@
 import { Link as RouteLink, useNavigate } from "react-router-dom"
 import { Box, Stack, Image, Input, InputGroup, InputRightElement, Button, IconButton, Link as ChakraLink, Icon, HStack, Text } from "@chakra-ui/react"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SearchIcon } from "@chakra-ui/icons";
 import { BsMoonStars, BsSun } from "react-icons/bs"
 import { useDispatch, useSelector } from "react-redux";
@@ -10,6 +10,7 @@ const Navbar = () => {
     const [search, setSearch] = useState("");
     const theme = useSelector((store) => store.authReducer.theme);
     const navigate = useNavigate();
+    // const [isAuth, setIsAuth] = useState(false);
     let isAuth = false;
     if (localStorage.getItem("token")) {
         isAuth = true;
@@ -32,7 +33,7 @@ const Navbar = () => {
         <Stack w={"90%"} m={"auto"} direction={"row"} display={"flex"} justifyContent={"space-between"} alignItems={"center"} p={"1"}>
             <Image src="https://firebasestorage.googleapis.com/v0/b/festive-crayon.appspot.com/o/Posts%2FCraft_Ideas1-removebg.png029b92d9-75e9-48d1-b100-5b790f007a6c?alt=media&token=b496407b-289a-45d6-952d-9bb82d176b81" alt="logo" objectFit={"cover"} w={"10%"} _hover={{ cursor: "pointer" }} onClick={() => { navigate("/") }} />
             <InputGroup w={"50%"}>
-                <Input value={search} placeholder="Search" borderRadius={"none"} focusBorderColor="#8FDBA7" borderColor={theme === "dark" ? "white" : "black"} _hover={{ borderColor: "none" }} onChange={(e) => setSearch(e.target.value)} />
+                <Input value={search} placeholder="Search" borderRadius={"none"} focusBorderColor="#8FDBA7" borderColor={theme == "dark" ? "white" : "black"} _hover={{ borderColor: "none" }} onChange={(e) => setSearch(e.target.value)} />
                 <InputRightElement >
                     <IconButton icon={<SearchIcon />} size={"sm"} p={"1"} bgColor={"#8FDBA7"} _hover={{ bgColor: "none" }} />
                 </InputRightElement>
