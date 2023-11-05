dec0f86a493b46a2e19fa076698e0deab077d997
Paras <paraskamliya5@gmail.com>
2023-11-05 09:09:53 +0530

    category update and grid update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <AllRoutes /> */}
-      <ArtPage />
+      <AllRoutes />
+      {/* <ArtPage /> */}
     </div>
   );
 }
