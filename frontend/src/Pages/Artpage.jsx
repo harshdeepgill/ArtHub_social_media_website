0419ad40929e3f8d4e1c8dd4c7f2a5bbaef57dc3
Paras <paraskamliya5@gmail.com>
2023-11-05 13:57:24 +0530

    update

--- frontend/src/Pages/Artpage.jsx
+++ frontend/src/Pages/Artpage.jsx
@@ -1,9 +1,11 @@
-import { Box, Button, Flex, Grid, GridItem, Image, Stack, Text, VStack } from "@chakra-ui/react";
+import { Box, Button, Center, Flex, Grid, GridItem, Heading, Image, Stack, Text, VStack } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { ViewIcon, StarIcon } from "@chakra-ui/icons"
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 const ArtPage = () => {
     const [curr, setCurr] = useState("");
     const [data, setData] = useState();
@@ -14,7 +16,7 @@ const ArtPage = () => {
     }
     const fetchTheData = () => {
         let url;
-        if (curr === "") {
+        if (curr == "") {
             url = "https://gifted-kit-cow.cyclic.app/arts"
         }
         else {
@@ -30,11 +32,12 @@ const ArtPage = () => {
             .then((res) => setData(res.reverse()))
             .catch((err) => console.log(err))
     }
+    console.log(data)
     const renderData1 = () => {
         const ele = []
-        for (let i = 0; i < data.length; i += 4) {
+        for (let i = 0; i < data?.length; i += 4) {
             ele.push(<Box key={data[i]._id} className={`div`} onClick={() => handleView(data[i]._id, data[i].views)}>
-                <Image src={data[i].image} className="div-img" />
+                <LazyLoadImage src={data[i].image} className="div-img" alt={data[i].title} effect="blur" />
                 <Text className="title">{data[i].title}</Text>
                 <VStack className="opacity">
                     <Text display={"flex"} alignItems={"center"} gap={"5px"}>{data[i].views} <ViewIcon /></Text>
@@ -46,9 +49,9 @@ const ArtPage = () => {
     }
     const renderData2 = () => {
         const ele = [];
-        for (let i = 1; i < data.length; i += 4) {
+        for (let i = 1; i < data?.length; i += 4) {
             ele.push(<Box key={data[i]._id} className={`div`} onClick={() => handleView(data[i]._id, data[i].views)}>
-                <Image src={data[i].image} className="div-img" />
+                <LazyLoadImage src={data[i].image} className="div-img" alt={data[i].title} effect="blur" />
                 <Text className="title">{data[i].title}</Text>
                 <VStack className="opacity">
                     <Text display={"flex"} alignItems={"center"} gap={"5px"}>{data[i].views} <ViewIcon /></Text>
@@ -60,9 +63,9 @@ const ArtPage = () => {
     }
     const renderData3 = () => {
         const ele = [];
-        for (let i = 2; i < data.length; i += 4) {
+        for (let i = 2; i < data?.length; i += 4) {
             ele.push(<Box key={data[i]._id} className={`div`} onClick={() => handleView(data[i]._id, data[i].views)}>
-                <Image src={data[i].image} className="div-img" />
+                <LazyLoadImage src={data[i].image} className="div-img" alt={data[i].title} effect="blur" />
                 <Text className="title">{data[i].title}</Text>
                 <VStack className="opacity">
                     <Text display={"flex"} alignItems={"center"} gap={"5px"}>{data[i].views} <ViewIcon /></Text>
@@ -74,9 +77,9 @@ const ArtPage = () => {
     }
     const renderData4 = () => {
         const ele = [];
-        for (let i = 3; i < data.length; i += 4) {
+        for (let i = 3; i < data?.length; i += 4) {
             ele.push(<Box key={data[i]._id} className={`div`} onClick={() => handleView(data[i]._id, data[i].views)}>
-                <Image src={data[i].image} className="div-img" />
+                <LazyLoadImage src={data[i].image} className="div-img" alt={data[i].title} effect="blur" />
                 <Text className="title">{data[i].title}</Text>
                 <VStack className="opacity">
                     <Text display={"flex"} alignItems={"center"} gap={"5px"}>{data[i].views} <ViewIcon /></Text>
@@ -110,11 +113,6 @@ const ArtPage = () => {
     useEffect(() => {
         fetchTheData();
     }, [curr])
-    if (data == null) {
-        return <Flex height="80vh" align="center" justify="center">
-            <Image src="https://cdn.slidevision.io/www/14304000000085015_loader.gif" w={"10%"} />
-        </Flex>
-    }
     return <Box w={"100%"} bgColor={theme == "dark" ? "#15191E" : "#edf2f7"} color={theme == "dark" ? "white" : "black"}>
         <Box w={"90%"} m={"auto"} padding={"1rem"}>
             <Grid w={"100%"} m={"auto"} templateColumns={["repeat(2,1fr)", "repeat(3,1fr)", "repeat(5,1fr)", "repeat(7,1fr)", "repeat(9,1fr)"]} columnGap={"1rem"} rowGap={"0.5rem"} templateRows={"auto"} p={"1rem"}>
@@ -146,6 +144,12 @@ const ArtPage = () => {
                     <Button w={"100%"} variant={"outline"} borderColor={curr == "Photography" ? "#00ff51" : "#ffb89e"} _hover={{ opacity: 0.8 }} colorScheme={"#15191E"} onClick={handleClick} borderRadius={"5px"} value={"Photography"}>Photography</Button>
                 </GridItem>
             </Grid>
+            {!data && <Flex height="80vh" align="center" justify="center">
+                <Image src="https://i.ibb.co/68zbKqX/output-onlinegiftools.gif" w={"10%"} />
+            </Flex>}
+            {data?.length == 0 && <Box>
+                <Heading textAlign={"center"}>There is no data present.</Heading>
+            </Box>}
             <Grid gap={"1rem"} templateColumns={["repeat(1,1fr)", "repeat(1,1fr)", "repeat(2,1fr)", "repeat(2,1fr)", "repeat(4,1fr)"]}>
                 <GRID>
                     <GridItem className="grid">
